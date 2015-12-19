3288185e6b9a9708edd41490e6a7a6cff3b625e0
Flo <flo69001@hotmail.fr>
2015-12-19 20:57:26 +0100

    - parse implementation

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,12 @@
  * Created by Flo on 14/12/2015.
  */
 
+Parse.initialize("9EmkHNqIHCenABISKjzBN1dH5T4qqpPK2lmJj3A6", "o7MjTIKFCEuLCSngoJuqMll4W5NxCxuow1xb57yX");
+
+var FavoriteList = Parse.Object.extend("FavoriteList");
+var favoriteList = new FavoriteList();
+
+
 var tilo = angular.module("tilo", ['http-post-fix', 'ngDraggable']);
 
 tilo.controller("tiloController", ["$scope", "$http", function($scope, $http) {
@@ -19,10 +25,35 @@ tilo.controller("tiloController", ["$scope", "$http", function($scope, $http) {
 
         var index = $scope.droppedObjects.indexOf(data);
 
-        if (index == -1)
+        if (index == -1) {
             $scope.droppedObjects.push(data);
+        }
+
+        for (var n = 0 ; n < $scope.droppedObjects.length ; n++) {
+            favoriteList.addUnique("film", $scope.droppedObjects[n].title);
+        }
+
+        favoriteList.addUnique("film", data.title);
+        favoriteList.save();
 
         console.log($scope.droppedObjects);
+
+        var query = new Parse.Query(FavoriteList);
+
+        query.find({
+            success: function(results) {
+                console.log("Successfully retrieved " + results.length + " titles.");
+                // Do something with the returned Parse.Object values
+                for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+                    console.log(object.id + ' - ' + object.get('film'));
+                    //object.destroy();
+                }
+            },
+            error: function(error) {
+                console.error("Error: " + error.code + " " + error.message);
+            }
+        });
     };
 
     $scope.onDragStart = function(data, evt) {
@@ -94,10 +125,40 @@ tilo.controller("tiloController", ["$scope", "$http", function($scope, $http) {
         $http.get("movies.json")
             .success(function(data) {
                 $scope.movies = data;
-                console.log($scope.movies);
             });
 
     }();
 
+    $scope.loadFavorites = function() {
 
+        var datas = [];
+
+        var query = new Parse.Query(FavoriteList);
+
+        query.find({
+            success: function(results) {
+                console.log("Successfully retrieved " + results.length + " titles.");
+                // Do something with the returned Parse.Object values
+                for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+                    console.log(object.id + ' - ' + object.get('film'));
+
+                    if (i < results.length - 1) { object.destroy(); }
+
+                    datas.push(object.get("film"));
+                }
+
+                $scope.favorites = datas[datas.length-1];
+                for (var j = 0 ; j < $scope.favorites.length ; j++) {
+                    $scope.droppedObjects.push({title : $scope.favorites[j]});
+                }
+
+                console.log($scope.droppedObjects);
+            },
+            error: function(error) {
+                console.error("Error: " + error.code + " " + error.message);
+            }
+        });
+
+    }();
 }]);
